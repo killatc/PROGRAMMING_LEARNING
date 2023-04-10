48c15d8288209d93801cf7c3626d06e76b6c63dd
Edith Tc <edith.cuadros@inpe.br>
2023-04-10 19:32:09 -0300

    Loop through Lists and Tuples

--- intro/loop_01.py
+++ intro/loop_01.py
@@ -24,10 +24,25 @@ print('fruit is: {}'.format(fruits[2]))
 
 
 
-fruits_list = ['apple', 'pear', 'pinaple', 'banana']
-fruits_list.append('mango')
-print('Fruit from list is: {}'.format(fruits_list[4]))
+fruits_list = ['Apple', 'Pear', 'Pinaple', 'Banana']
+fruits_list.append('Mango')
+print('Fruit from list is: {}'.format(fruits_list))
+print('Running List')
 
 
-fruits_tuple = ('apple', 'pear', 'pinaple', 'banana') 
-print('Fruit from tuple is: {}'.format(fruits_tuple[2]))
+# 1 - apple
+# 2 - pear
+# 3 - pinaple
+#...
+len_list = len(fruits_list)
+for i in range(0, len_list):
+    print('{} - {}'.format(i+1, fruits_list[i]))
+
+
+fruits_tuple = ('Peach', 'Blueberry', 'Grape', 'Orange') 
+print('Fruit from tuple is: {}'.format(fruits_tuple))
+
+print('Running Tuple')
+len_tuple = len(fruits_tuple)
+for i in range(0, len_tuple):
+    print('{} - {}'.format(i+len_list+1, fruits_tuple[i]))
