6b1c364aee2af3e7e5ce7089cbcc3efbf051a3b9
Edith Tc <edith.cuadros@inpe.br>
2023-04-19 18:42:55 -0300

    Loop Using While

---
+++ intro/loop_05.py
@@ -0,0 +1,23 @@
+"""
+LOOP USING FOR
+
+for index in list:
+    commands to execute in loop
+ex.:
+for element_of_list in [1, 2, 3, 'a', 'b']:
+    print(element_of_list)
+
+LOOṔ USING WHILE
+
+while stop_condition:
+    commands to execute in loop
+"""
+print('LOOP USING FOR')
+for i in range(1,11):
+    print(i)
+
+print("LOOP USING WHILE")
+j = 1
+while j <= 10:
+    print(j)
+    j+=1
