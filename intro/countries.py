00dfe34d0e49e3aecc7ea92f22db9b45fa5a83d9
Edith Tc <edith.cuadros@inpe.br>
2023-04-19 18:46:01 -0300

    Structured Dictionary Studies

---
+++ intro/countries.py
@@ -0,0 +1,25 @@
+countries = {
+    'Australia': {
+        'animals': ['Canguru', 'Deer', 'Moose', 'Snake'],
+        'cities': ['Sidney', 'Melbourne', 'Canberra']
+    },
+    'Brazil': {
+        'regions': {
+            'north': {
+                'states': {
+                    'Para': {
+                        'cities': ['Itaituba', 'Belém', 'Santarém', 'Oriximiná']
+                    },
+                    'Roraima': {
+                        'cities': ['Boa vista', 'Mucajaí', 'Bonfim']
+                    }
+                }
+            },
+            'south': {
+                'Paraná': {
+                    'cities': ['Curitiba']
+                }
+            }
+        }
+    }
+}
