e2d25b1b4c60951fd694cd27f9fc367cb6a184aa
Edith Tc <edith.cuadros@inpe.br>
2023-05-29 10:19:23 -0500

    Numpy_Plots using Matplotlib and Seaborn

---
+++ intro/modules_01.py
@@ -0,0 +1,9 @@
+import math
+import random
+import itertools # interação
+import typing    # tipicar que tipo  de estructura de datos es
+import os # comando de sistema operacional, deltar pastas, listar arquivos de um directorio, etc
+
+
+x = math.sqrt(25)
+print(x)
